e27102884b3ccdd7fe1d542faec1a00023e1020e
AmitkumarBR <amitrajanal59@gmail.com>
2021-07-22 23:49:57 +0530

    initial commit

---
+++ app/src/main/java/com/example/movieapp/DetailActivity.java
@@ -0,0 +1,83 @@
+package com.example.movieapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.squareup.picasso.Picasso;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView nameOfMovie, plotSynopsis, userRating, releaseDate;
+    ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        initCollapsingToolbar();
+
+        imageView = (ImageView) findViewById(R.id.thumbnail_image_header);
+        nameOfMovie = (TextView) findViewById(R.id.title);
+        plotSynopsis = (TextView) findViewById(R.id.plotSynopsis);
+        userRating = (TextView) findViewById(R.id.userrating);
+        releaseDate = (TextView) findViewById(R.id.releasedate);
+
+        Intent intent = getIntent();
+
+        if (intent.hasExtra("original_title")) {
+            String thumbnail = getIntent().getExtras().getString("poster_path");
+            String movieName = getIntent().getExtras().getString("original_title");
+            String synopsis = getIntent().getExtras().getString("overview");
+            String rating = getIntent().getExtras().getString("vote_average");
+            String dateOfRelease = getIntent().getExtras().getString("release_date");
+
+            Picasso.get().load(thumbnail).into(imageView);
+
+            nameOfMovie.setText(movieName);
+            plotSynopsis.setText(synopsis);
+            userRating.setText(rating);
+            releaseDate.setText(dateOfRelease);
+        } else {
+            Toast.makeText(this, "No API Data", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initCollapsingToolbar() {
+        final CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("");
+        AppBarLayout appBarLayout = findViewById(R.id.appbar);
+        appBarLayout.setExpanded(true);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle(getString(R.string.movie_details));
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbarLayout.setTitle("");
+                    isShow = false;
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/adapter/MoviesAdapter.java
@@ -0,0 +1,87 @@
+package com.example.movieapp.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.movieapp.DetailActivity;
+import com.example.movieapp.R;
+import com.example.movieapp.model.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Movie> movieList;
+
+    public MoviesAdapter(Context mContext, List<Movie> movieList) {
+        this.mContext = mContext;
+        this.movieList = movieList;
+    }
+
+    @NonNull
+    @Override
+    public MoviesAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_card, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesAdapter.MyViewHolder holder, int position) {
+        holder.title.setText(movieList.get(position).getOriginalTitle());
+        String vote = Double.toString(movieList.get(position).getVoteAverage());
+        holder.userrating.setText(vote);
+
+        Picasso.get().load(movieList.get(position).getPosterPath()).into(holder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView title, userrating;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view) {
+            super(view);
+
+            title = (TextView) view.findViewById(R.id.title);
+            userrating = (TextView) view.findViewById(R.id.userrating);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION) {
+                        Movie clickedDataItem = movieList.get(pos);
+
+                        Intent intent = new Intent(mContext, DetailActivity.class);
+                        intent.putExtra("original_title", movieList.get(pos).getOriginalTitle());
+                        intent.putExtra("poster_path", movieList.get(pos).getPosterPath());
+                        intent.putExtra("overview", movieList.get(pos).getOverview());
+                        intent.putExtra("vote_average", movieList.get(pos).getVoteAverage());
+                        intent.putExtra("release_date", movieList.get(pos).getReleaseDate());
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        mContext.startActivity(intent);
+                        Toast.makeText(v.getContext(), "You clicked" + clickedDataItem.getOriginalTitle(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+}
